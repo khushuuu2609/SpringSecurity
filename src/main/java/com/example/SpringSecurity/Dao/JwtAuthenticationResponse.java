765176e471f24adef090c4bf89543c31ac009b6c
khushboo <khushboodesai2609@gmail.com>
2024-03-21 23:00:54 +0530

    new changes..

--- src/main/java/com/example/SpringSecurity/Dao/JwtAuthenticationResponse.java
+++ src/main/java/com/example/SpringSecurity/Dao/JwtAuthenticationResponse.java
@@ -1,5 +1,8 @@
 package com.example.SpringSecurity.Dao;
 
+import com.example.SpringSecurity.Entity.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,5 +17,8 @@ public class JwtAuthenticationResponse {
     private String token;
     private String error;
     private String userId;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
