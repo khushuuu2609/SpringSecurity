acb71290cf98e092a43de4559ae88c0a8833f69d
khushboo <khushboodesai2609@gmail.com>
2024-04-10 19:11:40 +0530

    ntification

--- src/main/java/com/example/SpringSecurity/Config/EmailService.java
+++ src/main/java/com/example/SpringSecurity/Config/EmailService.java
@@ -14,10 +14,10 @@ import java.util.Properties;
 public class EmailService {
     public boolean sendEmail(String subject, String message, String to) {
         boolean f = false;
-        String from = "dhondetisha@gmail.com";
+        String from = "khushboodesai2609@gmail.com";
         String host = "smtp.gmail.com";
-        String username = "dhondetisha@gmail.com";
-        String password = "fqhb inul awab mnec";
+        String username = "khushboodesai2609@gmail.com";
+        String password = "sxjt ppwc jknm xerq";
 
         Properties properties = new Properties();
         properties.put("mail.smtp.host", host);
@@ -47,5 +47,5 @@ public class EmailService {
             e.printStackTrace();
         }
         return f;
-        }
+    }
 }
